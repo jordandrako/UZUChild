b1a48757f9a8567caabef20a53c3192e7ebbfaa9
Jordan Janzen <jordancjanzen@gmail.com>
2015-11-04 14:26:13 -0700

    Fix sample local config

--- lib/sample/local-config-sample.php
+++ lib/sample/local-config-sample.php
@@ -2,14 +2,14 @@
 // Local server settings
 
 // Local Database
-define('DB_NAME', 'wordpress');
-define('DB_USER', 'jordan');
-define('DB_PASSWORD', '1234');
+define('DB_NAME', 'database');
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'password');
 define('DB_HOST', 'localhost');
 
 // Overwrites the database to save keep editing the DB
-define('WP_HOME','http://localhost/efl');
-define('WP_SITEURL','http://localhost/efl');
+define('WP_HOME','http://localhost/site/');
+define('WP_SITEURL','http://localhost/site/');
 
 // Turn on debug for local environment
 define('WP_DEBUG', true);
